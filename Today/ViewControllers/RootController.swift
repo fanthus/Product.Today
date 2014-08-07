2e945826c7d5d049f32ff38c0c8af60d4207ea8b
fanxiushan <fanxiushan@gmail.com>
2014-08-07 22:16:53 +0800

    Today Project init

---
+++ Today/ViewControllers/RootController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by xiushan.fan on 7/8/14.
+//  Copyright (c) 2014年 Frank. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blueColor();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
